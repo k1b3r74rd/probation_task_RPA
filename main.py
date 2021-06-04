1748ef9ca675d231c7f082c79a3b328f9f7e855f
k1b3r74rd <kibertard@gmail.com>
2021-06-04 22:11:20 +0700

    finished excel, optimised libraries a bit

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 import requests, openpyxl
-import pandas as pd
 from bs4 import BeautifulSoup
 
+# Параметры для ссылки
 cur_usd = 'USD/RUB'
 cur_eur = 'EUR/RUB'
 mom_st = '2021-05-01'
@@ -9,10 +12,12 @@ mom_end = '2021-06-01'
 
 url_pattern = 'https://www.moex.com/export/derivatives/currency-rate.aspx?language=ru' \
               '&currency={cur}&moment_start={mom_st}&moment_end={mom_end}'
+
 url_usd = url_pattern.format(cur=cur_usd, mom_st=mom_st, mom_end=mom_end)
 url_eur = url_pattern.format(cur=cur_eur, mom_st=mom_st, mom_end=mom_end)
 
 
+# Конфигурация для правильной расстановки данных по столбикам.
 class Currency:
     def link(cur):
         if cur == 'usd':
@@ -39,16 +44,7 @@ class Currency:
             return 6
 
 
-wb = openpyxl.Workbook()
-sheet = wb['Sheet']
-sheet.cell(row=1, column=1).value = 'Дата Доллар'
-sheet.cell(row=1, column=2).value = 'Курс Доллар'
-sheet.cell(row=1, column=3).value = 'Изменение Доллар'
-sheet.cell(row=1, column=4).value = 'Дата Евро'
-sheet.cell(row=1, column=5).value = 'Курс Евро'
-sheet.cell(row=1, column=6).value = 'Изменение Евро'
-
-
+# Получение информации с сайта с последующей записью в excel.
 def excel_filler(cur):
     res = requests.get(Currency.link(cur))
     html_str = str(res.text)
@@ -56,22 +52,39 @@ def excel_filler(cur):
 
     rates = soup.find_all('rate')
 
+    format_cur_ruble = '#,##0.00_-₽'
     values = []
     for row, rate in enumerate(rates):
         data = rate.attrs
         cell_moment = sheet.cell(row=row + 2, column=Currency.column_moment(cur))
         cell_value = sheet.cell(row=row + 2, column=Currency.column_value(cur))
         cell_moment.value = data['moment']
+        cell_value.number_format = format_cur_ruble
         cell_value.value = float(data['value'])
         values.append(data['value'])
 
     for position in range(len(values)-1):
         value1, value2 = float(values[position+1]), float(values[position])
-        cell_dif = sheet.cell(row=position+2, column=Currency.column_dif(cur))
-        cell_dif.value = value2 - value1
+        cell_change = sheet.cell(row=position+2, column=Currency.column_dif(cur))
+        cell_change.value = value2 - value1
+        cell_change.number_format = format_cur_ruble
 
 
-excel_filler('usd')
-excel_filler('eur')
+# Деление курса евро на доллар и запись результата в столбик 'G'.
+def eur_on_dollar():
+    for pos in range(2, sheet.max_row+1):
+        eur_value = float(sheet.cell(row=pos, column=5).value)
+        usd_value = float(sheet.cell(row=pos, column=2).value)
+        sheet.cell(row=pos, column=7).value = eur_value / usd_value
 
-wb.save('test.xlsx')
+
+if __name__ == '__main__':
+    # pattern.xlsx - декоративный шаблон
+    wb = openpyxl.load_workbook('pattern.xlsx')
+    sheet = wb.active
+
+    excel_filler('usd')
+    excel_filler('eur')
+    eur_on_dollar()
+
+    wb.save('Динамика курса за прошлый месяц.xlsx')
